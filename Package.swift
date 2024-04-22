efb83cdb49b4200eeac3a8aeae146288d7a1a6d9
Łukasz Bożek <bozek.lu@gmail.com>
2024-04-22 15:19:52 +0200

    use revision SHA for PlCrashReporter

--- Package.swift
+++ Package.swift
@@ -68,7 +68,7 @@ let package = Package(
             targets: ["AppCenterCrashes"])
     ],
     dependencies: [
-        .package(url: "https://github.com/microsoft/plcrashreporter.git", .upToNextMinor(from: "1.11.2")),
+        .package(url: "https://github.com/microsoft/plcrashreporter.git", .revision("a7f5a174258d07f6913f48775418698b3e4618c1")),
     ],
     targets: [
         .target(
